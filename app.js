0c822fc92e11c8670f00e577eafc6a86d1a3ab1c
nzm-www <nizomiddinabdusattorov50@gmail.com>
2024-03-05 17:05:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,122 @@
+// let num1 = +prompt("asos");
+// let num2 = +prompt("balandlik");
+
+// function uchburchakYuzi(asos, balandlik) {
+//   let natija = (asos * balandlik) / 2;
+//   return natija;
+// }
+
+// console.log(uchburchakYuzi(num1, num2));
+
+// let ago = +prompt("yosh kirtin");
+
+// function agoTowDays(ago) {
+//   let natija = ago * 365;
+//   return natija;
+// }
+
+// console.log(agoTowDays(ago));
+
+// 10
+// let num1 = +prompt("asos");
+// let num2 = +prompt("balandlik");
+
+// function uchburchakуenguzuntomoni(asos, balandlik) {
+//   let natija = asos + balandlik - 1;
+//   return natija;
+// }
+
+// console.log(uchburchakуenguzuntomoni(num1, num2));
+
+// 11
+// let num1 = +prompt("asos");
+// let num2 = +prompt("balandlik");
+
+// function kiritilgansoniqoldigi(asos, balandlik) {
+//   let natija = asos % balandlik;
+//   return natija;
+// }
+
+// console.log(kiritilgansoniqoldigi(num1, num2));
+
+//13
+// let matn = prompt("Matin kirting");
+
+// function desc(matn) {
+//   let natija = "something" + matn;
+//   return natija;
+// }
+
+// console.log(desc(matn));
+
+// 14
+// let son = +prompt("son kirtin");
+
+// function number(son) {
+//   let natija = son * son;
+//   return natija;
+// }
+
+// console.log(number(son));
+
+// 15
+// let son = +prompt("son kirtin");
+
+// function number(son) {
+//   let natija = son <= 0;
+//   return natija;
+// }
+
+// console.log(number(son));
+
+// 16
+// let son = +prompt("son kirtin");
+
+// function number(son) {
+//   let natija = (son - 2) * 180;
+//   return natija;
+// }
+
+// console.log(number(son));
+
+// 17
+// let num1 = +prompt("asos");
+
+// function kiritilgansoniqoldigi(asos, balandlik) {
+//   let natija = asos * 2;
+
+//   return natija;
+// }
+
+// console.log(kiritilgansoniqoldigi(num1));
+
+// let num2 = +prompt("balandlik");
+
+// function kiritilgansoniqoldigi(balandlik) {
+//   let natija = balandlik * 3;
+
+//   return natija;
+// }
+
+// console.log(kiritilgansoniqoldigi(num2));
+
+//18
+// let matn = prompt("Matin kirting");
+
+// function desc(matn) {
+//   let natija = "JavaScript" + matn;
+//   return natija;
+// }
+
+// console.log(desc(matn));
+
+// 19
+// let num1 = +prompt("asos");
+// let num2 = +prompt("balandlik");
+
+// function uchburchakуenguzuntomoni(asos, balandlik) {
+//   let natija = asos + balandlik <= 100;
+//   return natija;
+// }
+
+// console.log(uchburchakуenguzuntomoni(num1, num2));
